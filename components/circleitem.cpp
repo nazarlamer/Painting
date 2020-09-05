78aebcc76993dfc612ef7e07e4ed9561b37f7a3a
Yurii Khomyk <khomykyu@gmail.com>
2020-09-05 14:00:21 +0300

    Implement selection for Circle and deleting components

--- components/circleitem.cpp
+++ components/circleitem.cpp
@@ -13,6 +13,31 @@ QRectF CircleItem::boundingRect() const
 void CircleItem::paint(QPainter *painter, const QStyleOptionGraphicsItem */*option*/,
                   QWidget */*widget*/)
 {
+    if (isSelected())
+        paintSelected(painter);
+    else
+        paintNotSelected(painter);
+}
+
+ComponentType CircleItem::componentType() const
+{
+    return ComponentType::Circle;
+}
+
+void CircleItem::paintSelected(QPainter *painter)
+{
+    painter->setPen(QPen(Qt::blue, 1));
+    painter->setBrush(Qt::NoBrush);
+    painter->drawRect(QRect(-20, -20, 40, 40));
+
+    painter->setPen(QPen(Qt::blue, 3));
+    painter->setBrush(Qt::SolidPattern);
+    painter->setBrush(Qt::yellow);
+    painter->drawEllipse(QRect(-20, -20, 40, 40));
+}
+
+void CircleItem::paintNotSelected(QPainter *painter)
+{
     painter->setPen(QPen(Qt::red, 1));
     painter->setBrush(Qt::NoBrush);
     painter->drawRect(QRect(-20, -20, 40, 40));
@@ -21,10 +46,5 @@ void CircleItem::paint(QPainter *painter, const QStyleOptionGraphicsItem */*opti
     painter->setBrush(Qt::SolidPattern);
     painter->setBrush(Qt::yellow);
     painter->drawEllipse(QRect(-20, -20, 40, 40));
-}
-
-ComponentType CircleItem::componentType() const
-{
-    return ComponentType::Circle;
 }
 
