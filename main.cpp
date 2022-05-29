43d5203253a60a69c41a9c2640d30f0ccdf7d71e
Nazarlamer <Dubbing@ukr.net>
2022-05-29 14:33:27 +0300

    run parse command line

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 /*Стабільний коміт 23.05.2022*/
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QStringList args = a.arguments();
+    //if (args.count() != 2)
+    //{
+        //std::cerr << "argument required" << endl;
+        //return 1;
+    //qDebug() << args;
+    //}
+    if (args.count() == 2)
+        w.setFileNameCommandLine(QString(args[1]));
+
     w.show();
     return a.exec();
 }
