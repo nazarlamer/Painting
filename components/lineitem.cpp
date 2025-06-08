41b470b1d373160138c44c0edffaf2322847dc3e
Nazarlamer <Dubbing@ukr.net>
2025-06-08 12:31:57 +0300

    revert MASTER

--- components/lineitem.cpp
+++ components/lineitem.cpp
@@ -1,15 +1,12 @@
 #include "lineitem.h"
 #include <QPainter>
-#include <QDebug>
 
 LineItem::LineItem(int id) : GrawItem(id)
 {
-
 }
 
 QRectF LineItem::boundingRect() const
 {
-    qDebug() << "LineItem::boundingRect()";
     return QRectF(-10, -10, 20, 20);
 }
 
